c8e9dcf66086ca8d7fb854e820bb9c34748813e8
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2020-06-29 15:29:59 +0200

    added comments

--- app/services/stripe_checkout_session_completed_service.rb
+++ app/services/stripe_checkout_session_completed_service.rb
@@ -1,9 +1,15 @@
 class StripeCheckoutSessionCompletedService
   def call(event)
+    # retrieve subscription data
     user = User.find_by(id: event.data.object.client_reference_id)
+
     subscription_stripe = Stripe::Subscription.retrieve(event.data.object.subscription)
+
     price_stripe_id = subscription_stripe.plan.id
+
     price = Price.find_by(stripe_id: price_stripe_id)
+
+    # create subscription
     subscription = Subscription.new(user: user, price: price, stripe_id: subscription_stripe.id, start_date: Time.at(subscription_stripe.start_date), status: "active")
     subscription.save
   end
