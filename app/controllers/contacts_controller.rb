443231bf7d5e4457849336ce8d8160d688d1ba9a
claire gautier <claire.gautier@edhec.com>
2020-07-03 11:54:35 +0200

    ajaxified contact form

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,8 @@ class ContactsController < ApplicationController
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
-      flash.now[:error] = "Votre message a bien été envoyé"
+      flash.now[:alert] = "Votre message a bien été envoyé"
+      redirect_to new_contact_path
     else
       flash.now[:error] = "An error has occured, the message cannot be sent."
       render :new
