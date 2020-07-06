c53096b01b23287176a452ac209277bace8b5e8e
claire gautier <claire.gautier@edhec.com>
2020-07-06 11:59:40 +0200

    Added i18n for all app and especially thredded

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
 
   # Do not eager load code on boot.
   config.eager_load = false
+  config.i18n.available_locales = [:en, :fr]
+
 
   # Show full error reports.
   config.consider_all_requests_local = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   mount StripeEvent::Engine, at: '/stripe-webhooks'
-  mount Thredded::Engine => '/forum'
-  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
-  root to: 'pages#home'
-  get '/equipe', to: 'pages#team'
-  get '/mon-profil/:id', to: 'users#show'
-  resources :contacts, only: [:new, :create]
-  resources :products, only: :index
-  resources :subscriptions, only: [:index, :destroy]
+  scope '(:locale)', locale: /#{I18n.available_locales.join("|")}/ do
+    mount Thredded::Engine => '/forum'
+    devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+    root to: 'pages#home'
+    get '/equipe', to: 'pages#team'
+    get '/mon-profil/:id', to: 'users#show'
+    resources :contacts, only: [:new, :create]
+    resources :products, only: :index
+    resources :subscriptions, only: [:index, :destroy]
+  end
 end
