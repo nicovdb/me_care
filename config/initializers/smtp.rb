3ec732e7649f0e273c2013a0fcca9f1523eb994d
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2020-07-06 16:02:12 +0200

    changed port

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,7 +1,7 @@
 ActionMailer::Base.smtp_settings = {
   domain: 'https://me-care-staging.herokuapp.com/',
   address:        "smtp.sendgrid.net",
-  port:            465,
+  port:            587,
   authentication: :plain,
   user_name:      'apikey',
   password:       ENV['SENDGRID_API_KEY']
