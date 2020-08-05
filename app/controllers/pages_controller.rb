4f6273ea680ad86b0050c08719690cf9b8150b34
claire gautier <claire.gautier@edhec.com>
2020-08-05 15:45:37 +0200

    footer desktop and static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,18 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :team, :legals, :policy, :conditions ]
 
   def home
   end
 
   def team
+  end
+
+  def legals
+  end
+
+  def policy
+  end
+
+  def conditions
   end
 end
