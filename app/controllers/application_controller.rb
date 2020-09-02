fd9dd8650393dd4c1d3ca282379272bb71936a85
claire gautier <claire.gautier@edhec.com>
2020-09-02 11:14:44 +0200

    finished both pages front

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,17 @@ class ApplicationController < ActionController::Base
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
   after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Uncomment when you *really understand* Pundit!
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  def configure_permitted_parameters
+    # For additional fields in app/views/devise/registrations/new.html.erb
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    # For additional in app/views/devise/registrations/edit.html.erb
+    #devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 
   def user_not_authorized
     flash[:alert] = "Vous n'êtes pas autorisée à faire cette action."
