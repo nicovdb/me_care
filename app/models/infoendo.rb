58379e9d5bd644f77b31d4c6a1fad12297153496
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2020-09-15 10:29:43 +0200

    validations for infoendos

--- app/models/infoendo.rb
+++ app/models/infoendo.rb
@@ -7,6 +7,11 @@ class Infoendo < ApplicationRecord
   enum media_type: { video: 0, article: 1 }
   enum category: { generalities: 0, diagnostic: 1, treatment: 2, food: 3, fertility: 4, radiography: 5, exam: 6, administrative: 7 }
 
+  validates :title, :content, :category, :media_type, :reading_time, :cover_credit, presence: true
+  validates :cover, attached: true
+  validates :media_type, inclusion: { in: media_types.keys }
+  validates :category, inclusion: { in: categories.keys }
+
   include PgSearch::Model
   pg_search_scope :search_by_content_and_title,
     against: [:title],
