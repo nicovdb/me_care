d27da7ea0148d24b752e5efd004aa944d439c95c
claire gautier <claire.gautier@edhec.com>
2020-09-15 12:06:54 +0200

    anonymized user when account deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user
 
   def show
-    @user = User.find(params[:id])
+
     authorize @user
+  end
+
+  def anonymize
+    @user.email = "#{@user.id}@email.com"
+    @user.first_name = "Account deleted"
+    @user.last_name = "Account deleted"
+    @user.save
+    authorize @user
+    sign_out
+    flash[:alert] = "Votre compte a bien été supprimé."
+    redirect_to root_path
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
