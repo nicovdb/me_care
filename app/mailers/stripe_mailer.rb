aef84fbdac2a320628e3d1f1f2c9150c79faf40e
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2020-10-12 16:36:04 +0200

    send an email when price is changed

--- app/mailers/stripe_mailer.rb
+++ app/mailers/stripe_mailer.rb
@@ -3,4 +3,11 @@ class StripeMailer < ApplicationMailer
     @user = params[:user]
     mail(to: @user.email, subject: "Votre période d'essai à easy Endo est terminée")
   end
+
+  def customer_changed_plan
+    @user = params[:user]
+    @duration = params[:duration]
+    @interval = params[:interval]
+    mail(to: @user.email, subject: "Vous avez changé d'abonnement")
+  end
 end
