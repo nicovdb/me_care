00dba81b0323319ad862b558ef85d481e39c5b66
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2020-10-14 10:17:03 +0200

    good convention

--- app/javascript/controllers/photo_preview_controller.js
+++ app/javascript/controllers/photo_preview_controller.js
@@ -1,14 +1,14 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = [ "photopreview", "currentavatar" ]
+  static targets = [ "photoPreview", "currentAvatar" ]
 
   display(event) {
     const input = event.target
     if (input.files && input.files[0]) {
       const reader = new FileReader()
-      const newAvatar = this.photopreviewTarget
-      const oldAvatar = this.currentavatarTarget
+      const newAvatar = this.photoPreviewTarget
+      const oldAvatar = this.currentAvatarTarget
       reader.onload = (event) => {
         newAvatar.src = event.currentTarget.result;
       }
