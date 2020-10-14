c947a0ce0a60a8bf256b22a6e2e1a648e2fe0c70
claire gautier <claire.gautier@edhec.com>
2020-10-14 17:33:52 +0200

    moved check trial end to payment failed webhook

--- app/services/stripe/customer_subscription_updated_service.rb
+++ app/services/stripe/customer_subscription_updated_service.rb
@@ -12,22 +12,10 @@ module Stripe
       @customer = Stripe::Customer.retrieve(@stripe_subscription.customer)
       @subscription = Stripe::Subscription.retrieve(@stripe_subscription.id)
 
-      check_trial_end
       check_change_price
     end
 
     private
-
-    def check_trial_end
-      cards_count = @customer['sources']['total_count']
-      now = DateTime.now
-      trial_start = Time.at(@subscription['start_date']).to_datetime
-      days_since = now.day - trial_start.day
-
-      if days_since == 15 && cards_count < 1
-        StripeMailer.with(user: @user).trial_ended_without_card.deliver_now
-      end
-    end
 
     def check_change_price
       if @event.data&.previous_attributes.keys.include?(:items)
--- app/services/stripe/invoice_payment_failed_service.rb
+++ app/services/stripe/invoice_payment_failed_service.rb
@@ -1,10 +1,21 @@
 module Stripe
   class InvoicePaymentFailedService
     def call(event)
-      @event = event
-      @stripe_customer = @event.data.object.customer
-      @user = User.find_by(stripe_id: @stripe_customer)
-      StripeMailer.with(user: @user).invoice_payment_failed.deliver_now
+      stripe_customer_id = event.data.object.customer
+      stripe_customer = Stripe::Customer.retrieve(stripe_customer_id)
+      stripe_subscription_id = event.data.object.subscription
+      stripe_subscription = Stripe::Subscription.retrieve(stripe_subscription_id)
+      cards_count = stripe_customer['sources']['total_count']
+      now = DateTime.now
+      trial_start = Time.at(stripe_subscription['start_date']).to_datetime
+      days_since = now.day - trial_start.day
+      user = User.find_by(stripe_id: stripe_customer_id)
+
+      if days_since == 15 && cards_count < 1
+        StripeMailer.with(user: user).trial_ended_without_card.deliver_now
+      else
+        StripeMailer.with(user: user).invoice_payment_failed.deliver_now
+      end
     end
   end
 end
