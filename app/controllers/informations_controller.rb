f509478de0af8960812c876581cbddfd4fda5786
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2020-10-22 13:11:48 +0200

    cleaned files

--- app/controllers/informations_controller.rb
+++ app/controllers/informations_controller.rb
@@ -1,5 +1,4 @@
 class InformationsController < ApplicationController
-
   before_action :set_user, only: [:new, :create, :edit, :update]
   before_action :set_information, only: [:update, :destroy]
 
@@ -13,16 +12,13 @@ class InformationsController < ApplicationController
   end
 
   def create
+    @information = Information.new(information_params)
+    authorize @information
     if information_params[:terms_conditions] == "0"
-      @information = Information.new(information_params)
-      authorize @information
       flash[:alert] = "Vous devez accepter le traitement de vos données."
       @anchor = "terms"
       render 'new'
     else
-      #check_for_other_disease_and_therapy
-      @information = Information.new(information_params)
-      authorize @information
       @information.user = @user
       if @information.save
         flash[:alert] = nil
@@ -35,7 +31,8 @@ class InformationsController < ApplicationController
 
   def edit
     @information = Information.includes(:diseases, :alternative_therapies).find(params[:id])
-    set_diseases_and_therapies
+    @displayed_diseases = Disease.where(displayed: true)
+    @displayed_therapies = AlternativeTherapy.where(displayed: true)
     authorize @information
   end
 
@@ -46,13 +43,8 @@ class InformationsController < ApplicationController
       @anchor = "terms"
       render 'edit'
     else
-      delete_info_diseases
-      delete_info_alternative_therapies
-      delete_info_fam_member_antes
-      delete_children_number
-      delete_miscarriage_number
-      delete_endo_surgery_number
-      delete_abortion_number
+      delete_infos
+      delete_form_numbers
       if @information.update(information_params)
         flash[:alert] = nil
         redirect_to profil_path
@@ -71,45 +63,30 @@ class InformationsController < ApplicationController
 
   private
 
-  def delete_info_diseases
+  def delete_infos
     if params[:information][:auto_immune_antecedent] == 'false'
       @information.info_diseases.destroy_all
       @information.other_disease = nil
     end
-  end
-
-  def delete_info_alternative_therapies
     if params[:information][:alternative_therapy] == 'false'
       @information.info_alternative_therapies.destroy_all
       @information.other_alternative_therapy = nil
     end
-  end
-
-  def delete_info_fam_member_antes
     if params[:information][:family_antecedent] == 'false'
       @information.info_fam_member_antes.destroy_all
     end
   end
 
-  def delete_children_number
+  def delete_form_numbers
     if params[:information][:children] == 'false'
       @information.children_number = nil
     end
-  end
-
-  def delete_miscarriage_number
     if params[:information][:miscarriage] == 'false'
       @information.miscarriage_number = nil
     end
-  end
-
-  def delete_endo_surgery_number
     if params[:information][:endo_surgery] == 'false'
       @information.endo_surgery_number = nil
     end
-  end
-
-  def delete_abortion_number
     if params[:information][:abortion] == 'false'
       @information.abortion_number = nil
     end
@@ -125,30 +102,5 @@ class InformationsController < ApplicationController
 
   def information_params
     params.require(:information).permit(:auto_immune_antecedent, :date_of_birth, :family_situation, :job, :diagnosis_age, :size, :weight, :imc, :family_antecedent, :children, :children_number, :abortion, :abortion_number, :pma, :endo_surgery, :endo_surgery_number, :pain_center, :physiotherapist, :ostheopath, :alternative_therapy, :terms_conditions, :miscarriage, :miscarriage_number, :other_alternative_therapy, :other_disease, {fam_member_ante_ids: [] }, {alternative_therapy_ids: [] }, {disease_ids: [] }, diseases_attributes: [:name], alternative_therapies_attributes: [:name])
-  end
-
-  # def check_for_other_disease_and_therapy
-  #   params_diseases = params[:information].dig(:diseases_attributes, "0", "name")
-  #   params_therapies = params[:information].dig(:alternative_therapies_attributes, "0", "name")
-  #   if params_diseases&.empty?
-  #     params[:information].delete :diseases_attributes
-  #   end
-  #   if params_therapies&.empty?
-  #     params[:information].delete :alternative_therapies_attributes
-  #   end
-  # end
-
-  def set_diseases_and_therapies
-    @displayed_diseases = Disease.where(displayed: true)#.to_a
-    # additional_diseases = @information.diseases.where(displayed: false)
-    # unless additional_diseases.empty? || additional_diseases.first.name.empty?
-    #   @displayed_diseases << additional_diseases.first
-    # end
-
-    @displayed_therapies = AlternativeTherapy.where(displayed: true)#.to_a
-    # additional_therapies = @information.alternative_therapies.where(displayed: false)
-    # unless additional_therapies.empty? || additional_therapies.first.name.empty?
-    #   @displayed_therapies << additional_therapies.first
-    # end
   end
 end
