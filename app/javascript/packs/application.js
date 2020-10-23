d082c6186b109468f6dfb72c9bd94b36f2695e39
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2020-10-23 17:52:18 +0200

    ok with thredded

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels");
-// require("thredded_imports.js");
+require("thredded_imports.js");
 require("trix");
 require("@rails/actiontext");
 require('packs/social-share-button.js.erb');
