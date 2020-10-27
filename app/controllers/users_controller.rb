5546cbc0bab7a4f84fa64dea9a229d74a9d5c47c
claire gautier <claire.gautier@edhec.com>
2020-10-27 10:42:52 +0100

    percentage profil completion progress bar calculation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,7 @@ class UsersController < ApplicationController
   def show
     @user = current_user
     authorize @user
+    calculate_profile_completion
     @favorites = @user.favorites.includes([:infoendo, :article])
   end
 
@@ -32,6 +33,62 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def calculate_profile_completion
+    profil_completion = 10
+    if @user.avatar.attached?
+      profil_completion += 10
+    end
+    if @user.information
+      if !@user.information.date_of_birth.nil?
+        profil_completion += 5.5
+      end
+      if !@user.information.family_situation.nil?
+        profil_completion += 5.5
+      end
+      if !@user.information.job.nil?
+        profil_completion += 5.5
+      end
+      if !@user.information.diagnosis_age.nil?
+        profil_completion += 5.5
+      end
+      if !@user.information.imc.nil?
+        profil_completion += 5.5
+      end
+      if !@user.information.family_antecedent.nil?
+        profil_completion += 5.5
+      end
+      if !@user.information.children.nil?
+        profil_completion += 5.5
+      end
+      if !@user.information.abortion.nil?
+        profil_completion += 5.5
+      end
+      if !@user.information.pma.nil?
+        profil_completion += 5.5
+      end
+      if !@user.information.endo_surgery.nil?
+        profil_completion += 5.5
+      end
+      if !@user.information.pain_center.nil?
+        profil_completion += 5
+      end
+      if !@user.information.physiotherapist.nil?
+        profil_completion += 5
+      end
+      if !@user.information.ostheopath.nil?
+        profil_completion += 5
+      end
+      if !@user.information.auto_immune_antecedent.nil?
+        profil_completion += 5
+      end
+      if !@user.information.alternative_therapy.nil?
+        profil_completion += 5
+      end
+    end
+    @profil_completion_round = profil_completion.round
+    @profil_completion_round
+  end
 
   def set_user
     @user = User.find(params[:id])
