99e31cf70e107766e84451154a755f11cb4ab96c
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2020-11-03 14:22:23 +0100

    added condition in webhook service checkout completed

--- app/services/stripe/checkout_session_completed_service.rb
+++ app/services/stripe/checkout_session_completed_service.rb
@@ -1,12 +1,13 @@
 module Stripe
   class CheckoutSessionCompletedService
     def call(event)
-      user = User.find_by(stripe_id: event.data.object.customer)
-      webinar = Webinar.find_by(title: event.data.object.display_items[0].custom.name)
-      amount = event.data.object.amount_total.to_i
-      @webinar_subscription = WebinarSubscription.new(user: user, webinar: webinar, state: "paid", amount_cents: amount)
-      @webinar_subscription.save
-
+      if event.data.object.mode == "payment"
+        user = User.find_by(stripe_id: event.data.object.customer)
+        webinar = Webinar.find_by(title: event.data.object.display_items[0].custom.name)
+        amount = event.data.object.amount_total.to_i
+        @webinar_subscription = WebinarSubscription.new(user: user, webinar: webinar, state: "paid", amount_cents: amount)
+        @webinar_subscription.save
+      end
     rescue StandardError => e
       channel = Rails.env.development? || Rails.env.production? ? 'DEVELOPMENT' : 'PRODUCTION'
       Zapier::StripeError.new({ event: event, error: e, channel: channel, service: "StripeCheckoutSessionCompleted" }).post_to_zapier
