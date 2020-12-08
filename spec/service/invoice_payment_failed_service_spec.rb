9dda580e530b6b16f2a067135e7061304eaf9d78
claire gautier <claire.gautier@edhec.com>
2020-12-08 10:26:07 +0100

    Test invoice payment failed

--- spec/service/invoice_payment_failed_service_spec.rb
+++ spec/service/invoice_payment_failed_service_spec.rb
@@ -14,12 +14,12 @@ RSpec.describe Stripe::InvoicePaymentFailedService, type: :model do
       DatabaseCleaner.clean
     end
 
+    let(:user) { FactoryBot.create(:user, :confirmed) }
+
     it "detects when the invoice payment fails" do
-      #request = StripeMock.mock_webhook_event('invoice.payment_failed', customer: @user.stripe_id)
-      #mettre en place test c'est bon ça marche
-      #expect(request.data.object.customer).to eql @user.stripe_id
-      #expect(request.data.object.status).to eql @user.subscription.status
-      # probleme : quand je test la user.sub reste en trialing donc pas poss de la supp
-      #expect(@user.has_valid_subscription?).to eql false
+      before_count = ActionMailer::Base.deliveries.count
+      request = StripeMock.mock_webhook_event('invoice.payment_failed', customer: user.stripe_id)
+      described_class.new.call(request)
+      expect(ActionMailer::Base.deliveries.count).to be > (before_count)
     end
 end
