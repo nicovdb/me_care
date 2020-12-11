61aa43f9177172246f1d2c2876e4e5db0c9ed60b
claire gautier <claire.gautier@edhec.com>
2020-12-11 16:37:49 +0100

    added icons insomnia slider

--- app/javascript/controllers/range-slider-insomnia_controller.js
+++ app/javascript/controllers/range-slider-insomnia_controller.js
@@ -3,35 +3,7 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = ['range', 'value', 'hidden']
 
-  connect() {
-    if (this.hasValueTarget) {
-      this.updateText(this.rangeTarget.value)
-    }
-  }
-
   updateValue(e) {
-    this.updateText(e.currentTarget.value);
     this.hiddenTarget.value = e.currentTarget.value;
-  }
-
-  updateText(element) {
-    if (element == 0) {
-      this.valueTarget.innerHTML = "<img class='insomnia-icon mb-1' src='/assets/agenda/insomnia1.svg'>";
-    }
-    if (element == 1) {
-      this.valueTarget.innerHTML = "<img class='insomnia-icon mb-1' src='/assets/agenda/insomnia2.svg'>";
-    }
-    if (element == 2) {
-      this.valueTarget.innerHTML = "<img class='insomnia-icon mb-1' src='/assets/agenda/insomnia3.svg'>";
-    }
-    if (element == 3) {
-      this.valueTarget.innerHTML = "<img class='insomnia-icon mb-1' src='/assets/agenda/insomnia4.svg'>";
-    }
-    if (element == 4) {
-      this.valueTarget.innerHTML = "<img class='insomnia-icon mb-1' src='/assets/agenda/insomnia5.svg'>";
-    }
-    if (element == 5) {
-      this.valueTarget.innerHTML = "<img class='insomnia-icon mb-1' src='/assets/agenda/insomnia6.svg'>";
-    }
   }
 }
