190474bee5ae802369b84bc80751b8062a8c4e57
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2020-12-11 17:56:48 +0100

    charts working for week without filters

--- app/controllers/daily_symptoms_controller.rb
+++ app/controllers/daily_symptoms_controller.rb
@@ -2,7 +2,13 @@ class DailySymptomsController < ApplicationController
   def index
     define_table
     @daily_symptoms = policy_scope(DailySymptom).where('day >= ?', @first_day_of_month).where('day <= ?', @last_day_of_month)
-    define_data
+    define_agenda_data
+  end
+
+  def graph
+    authorize :daily_symptom
+    @daily_symptoms = policy_scope(DailySymptom)
+    define_graph_data
   end
 
   def new
@@ -79,7 +85,7 @@ class DailySymptomsController < ApplicationController
     params.require(:daily_symptom).permit(:pain_level, :blood_level, :digestive_trouble_level, :stress_level, :insomnia_level, :sport, :emergency, :analgesic, :notes, :day)
   end
 
-  def define_data
+  def define_agenda_data
     day = @first_day_of_month
     @data = []
 
@@ -89,6 +95,104 @@ class DailySymptomsController < ApplicationController
         @data << { day: day, daily_symptom: daily_symptom.id, symptoms: daily_symptom.symptoms_name_and_color }
       else
         @data << { day: day, daily_symptom: nil, symptoms: nil }
+      end
+      day += 1
+    end
+  end
+
+  def define_graph_data
+    # id"name", label"name", borderColor"#hex", data[array of values], suggestedMax 5 ou 10, stepSize 1 ou 2
+    @pain_data = []
+    @blood_data = []
+    @digestive_trouble_data = []
+    @stress_data = []
+    @insomnia_data = []
+    @sport_data = []
+
+    if params[:base_period].present?
+      if params[:base_period] == "month"
+        monthly_graph_data
+      elsif params[:base_period] == "week"
+        weekly_graph_data
+      elsif params[:base_period] == "trimester"
+        trimester_graph_data
+      elsif params[:base_period] == "year"
+        year_graph_data
+      else
+        monthly_graph_data
+      end
+    else
+      weekly_graph_data
+    end
+
+    @graphs = [
+      {
+        id: "painChart",
+        label: "Douleurs",
+        border_color: "#FDA55E",
+        data: @pain_data,
+        suggested_max: "10",
+        step_size: "2"
+      },
+      {
+        id: "bloodChart",
+        label: "Saignements",
+        border_color: "#DB9497",
+        data: @blood_data,
+        suggested_max: "5",
+        step_size: "1"
+      },
+      {
+        id: "digestiveTroubleChart",
+        label: "Troubles digestifs",
+        border_color: "#8C97CF",
+        data: @digestive_trouble_data,
+        suggested_max: "5",
+        step_size: "1"
+      },
+      {
+        id: "stressChart",
+        label: "Stress",
+        border_color: "#B9B9B9",
+        data: @stress_data,
+        suggested_max: "5",
+        step_size: "1"
+      },
+      {
+        id: "insomniaChart",
+        label: "Insomnie",
+        border_color: "#FFF185",
+        data: @insomnia_data,
+        suggested_max: "5",
+        step_size: "1"
+      }
+    ]
+  end
+
+  def weekly_graph_data
+    if params[:base_day].present?
+      monday = params[:base_day].to_date
+    else
+      monday = Date.today - (Date.today.cwday - 1)
+    end
+
+    day = monday
+    7.times do
+      daily_symptom = @daily_symptoms.find_by(day: day)
+      if daily_symptom
+        @pain_data << daily_symptom.pain_level
+        @blood_data << daily_symptom.blood_level
+        @digestive_trouble_data << daily_symptom.digestive_trouble_level
+        @stress_data << daily_symptom.stress_level
+        @insomnia_data << daily_symptom.insomnia_level
+        @sport_data << daily_symptom.sport
+      else
+        @pain_data << 0
+        @blood_data << 0
+        @digestive_trouble_data << 0
+        @stress_data << 0
+        @insomnia_data << 0
+        @sport_data << false
       end
       day += 1
     end
---
+++ app/javascript/controllers/charts_controller.js
@@ -0,0 +1,62 @@
+import { Controller } from "stimulus"
+import Chart from 'chart.js';
+
+export default class extends Controller {
+  static targets = [ "graphs" ]
+
+  connect() {
+    // id"name", label"name", borderColor"#hex", data[array of values], suggestedMax 5 ou 10, stepSize 1 ou 2
+    JSON.parse(this.graphsTarget.dataset.value).forEach((graph) => {
+      console.dir(`${graph["data"]}`)
+      this.displayChart(graph)
+    })
+  }
+
+  displayChart(graph) {
+    var ctx = document.getElementById(`${graph["id"]}`).getContext('2d');
+    var chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+
+      // The data for our dataset
+      data: {
+        labels: ['', '', '', '', ''],
+        datasets: [{
+          label: `${graph["label"]}`,
+          backgroundColor: 'transparent',
+          borderColor: `${graph["border_color"]}`,
+          borderWidth: 4,
+          data: Array.from(graph["data"]),
+          pointRadius: 7,
+          pointHoverRadius: 7,
+          pointBackgroundColor: '#EB7099',
+          pointBorderWidth: 0
+        }],
+      },
+
+      // Configuration options go here
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          yAxes: [{
+            display: true,
+            ticks: {
+                suggestedMax: `${graph["suggested_max"]}`,
+                beginAtZero: true,
+                stepSize: `${graph["step_size"]}`,
+                fontColor: '#B9B9B9',
+                padding: 10
+            },
+            gridLines: {
+                borderDash: [5, 5]
+            }
+          }]
+        },
+        legend: {
+          display: false
+        }
+      }
+    });
+  }
+}
