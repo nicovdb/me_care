d8c87aed340e79f1f306c82fdfa4a8524a88fa43
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2020-12-17 15:39:21 +0100

    export working

--- app/javascript/controllers/charts_controller.js
+++ app/javascript/controllers/charts_controller.js
@@ -3,33 +3,38 @@ import Chart from 'chart.js';
 import { jsPDF } from "jspdf";
 
 export default class extends Controller {
-  static targets = [ "graphs", "period" ]
+  static targets = [ "graphs", "period", "export" ]
 
   connect() {
-    this.initializePDF()
-
     JSON.parse(this.graphsTarget.dataset.value).forEach((graph) => {
-      this.symptomTitlePDF(graph)
       this.displayChart(graph)
     })
-    // this.doc.save("a4.pdf");
   }
 
-  initializePDF() {
-    this.doc = new jsPDF()
-    this.row = 10
-    this.doc.text(this.periodTarget.textContent, 10, this.row)
+  downloadPDF() {
+    const doc = new jsPDF()
+    const width = parseInt(this.exportTarget.dataset.graphWidth, 10) / 4
+
+    doc.text(this.periodTarget.textContent, 10, 10)
+
+    doc.text("Douleurs", 10, 30)
+    doc.addImage(this.exportTarget.dataset["painChart"], "PNG", 10, 35, width, 37.5)
+
+    doc.text("Saignements", 10, 80)
+    doc.addImage(this.exportTarget.dataset["bloodChart"], "PNG", 10, 85, width, 37.5)
+
+    doc.text("Troubles digestifs", 10, 130)
+    doc.addImage(this.exportTarget.dataset["digestiveTroubleChart"], "PNG", 10, 135, width, 37.5)
+
+    doc.text("Stress", 10, 180)
+    doc.addImage(this.exportTarget.dataset["stressChart"], "PNG", 10, 185, width, 37.5)
+
+    doc.text("Insomnie", 10, 230)
+    doc.addImage(this.exportTarget.dataset["insomniaChart"], "PNG", 10, 235, width, 37.5)
+
+    doc.save(`${this.periodTarget.textContent}.pdf`);
   }
 
-  symptomTitlePDF(graph) {
-    this.row = this.row + 20
-    this.doc.text(graph['name'], 10, this.row)
-    this.row = this.row + 10
-  }
-
-  symptomImagePDF(image) {
-    // this.doc.text(image, 10, this.row)
-  }
 
   toggle(e) {
     e.target.parentElement.classList.toggle("underline")
@@ -53,7 +58,6 @@ export default class extends Controller {
   }
 
   displayChart(graph) {
-    let graphDoc = this.doc
     var ctx = document.getElementById(`${graph["id"]}`).getContext('2d');
     Chart.defaults.global.defaultFontFamily = 'Lato';
     var chart = new Chart(ctx, {
@@ -81,7 +85,8 @@ export default class extends Controller {
       options: {
         animation: {
           onComplete: function() {
-            document.getElementById(`img-${graph["id"]}`).src = chart.toBase64Image();
+            document.getElementById("data-export").dataset["graphWidth"] = chart.canvas.style.width.slice(0, -2);
+            document.getElementById("data-export").dataset[graph["id"]] = chart.toBase64Image();
           }
         },
         responsive: true,
