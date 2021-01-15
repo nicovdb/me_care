9e2b72ee04d84d9d38ccccfd0f1fefcaffe5c568
jfschaff <jf.schaff@postelo.fr>
2021-01-15 14:22:51 +0100

    host in env variable DEFAULT_URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,5 @@
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "https://me-care-staging.herokuapp.com/" }
+  config.action_mailer.default_url_options = { host: ENV["DEFAULT_URL"] }
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.delivery_method = :smtp
   # Code is not reloaded between requests.
