346033a9cacbd378ae414d11a32801b4a9306637
claire gautier <claire.gautier@edhec.com>
2021-01-29 15:28:35 +0100

    change form to make user admin

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,6 +7,7 @@ class DashboardsController < ApplicationController
     @webinars = policy_scope(Webinar).order(start_at: :desc)
     @infoendos = Infoendo.all.order(publication_date: :desc)
     @admins = User.where(admin: true)
+    @non_admins = User.where(admin:false)
   end
 
   def publish_article
