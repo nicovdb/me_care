5c558024051e93ca8c713f5d251d8f2bbdc11af8
claire gautier <claire.gautier@edhec.com>
2021-02-01 14:35:40 +0100

    removed unecessary method

--- app/mailers/stripe_mailer.rb
+++ app/mailers/stripe_mailer.rb
@@ -9,14 +9,6 @@ class StripeMailer < ApplicationMailer
     mail(to: @user.email, subject: "Votre abonnement easy Endo est terminé")
   end
 
-  def customer_first_sub
-    @user = params[:user]
-    @duration = params[:duration]
-    @interval = params[:interval]
-    @subscription = params[:subscription]
-    mail(to: @user.email, subject: "Votre abonnement > easy endo en accès illimité !")
-  end
-
   def customer_changed_plan
     @user = params[:user]
     @duration = params[:duration]
