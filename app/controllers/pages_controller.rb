2f4e205335875d78bf2a8929eb750870208b0353
claire gautier <claire.gautier@edhec.com>
2021-02-03 17:43:56 +0100

    changed success url

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -36,7 +36,7 @@ class PagesController < ApplicationController
           quantity: 1
         }],
         mode: 'subscription',
-        success_url: profil_url,
+        success_url: 'https://me-care-staging.herokuapp.com/profil',
         cancel_url: products_url,
         client_reference_id: current_user.id,
         customer: find_or_create_stripe_customer_id
