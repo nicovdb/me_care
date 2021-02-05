708c1615aae23e98cd7950b08157b46a56892209
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2021-02-05 13:04:33 +0100

    return url from stripe billing

--- app/controllers/customer_portal_sessions_controller.rb
+++ app/controllers/customer_portal_sessions_controller.rb
@@ -3,7 +3,7 @@ class CustomerPortalSessionsController < ApplicationController
     authorize(:customer_portal_session, :create?)
     portal_session = Stripe::BillingPortal::Session.create(
       customer: current_user.stripe_id,
-      return_url: root_url
+      return_url: ENV["SUCCESS_URL_STRIPE"]
     )
     redirect_to portal_session.url
   end
