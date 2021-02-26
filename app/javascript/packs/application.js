94e8f7cfb5176d3449e3deaee92e400a6054e122
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2021-02-26 17:43:19 +0100

    added all but turbolinks

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,20 +1,20 @@
-// require("@rails/ujs").start();
+require("@rails/ujs").start();
 // require("turbolinks").start();
-// require("@rails/activestorage").start();
-// require("channels");
-// require("trix");
-// require("@rails/actiontext");
-// require('packs/social-share-button.js.erb');
+require("@rails/activestorage").start();
+require("channels");
+require("trix");
+require("@rails/actiontext");
+require('packs/social-share-button.js.erb');
 
 // External imports
-// import "bootstrap";
-// import "../plugins/flatpickr";
+import "bootstrap";
+import "../plugins/flatpickr";
 
 // Internal imports
-// import "controllers";
+import "controllers";
 // import { navbarMenu } from '../components/navbar_menu';
 
 // document.addEventListener('turbolinks:load', () => {
-  // navbarMenu();
+//   navbarMenu();
 // });
 
