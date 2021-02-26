b6a474577b2c06d516d257c3361f641115333d9e
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2021-02-26 17:31:48 +0100

    removed all in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
-require("@rails/ujs").start();
-require("turbolinks").start();
-require("@rails/activestorage").start();
-require("channels");
-require("trix");
-require("@rails/actiontext");
-require('packs/social-share-button.js.erb');
+// require("@rails/ujs").start();
+// require("turbolinks").start();
+// require("@rails/activestorage").start();
+// require("channels");
+// require("trix");
+// require("@rails/actiontext");
+// require('packs/social-share-button.js.erb');
 
 // External imports
 // import "bootstrap";
