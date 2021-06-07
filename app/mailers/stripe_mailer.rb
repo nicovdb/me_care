ce6be4854d94879c9460f4079d668cb0029f7599
claire gautier <claire.gautier@edhec.com>
2021-06-07 12:20:17 +0200

    removed all strip mailers

--- app/mailers/stripe_mailer.rb
+++ app/mailers/stripe_mailer.rb
@@ -1,24 +1,24 @@
 class StripeMailer < ApplicationMailer
-  def trial_ended_without_card
-    @user = params[:user]
-    mail(to: @user.email, subject: "Votre période d'essai à easy Endo est terminée")
-  end
+  # def trial_ended_without_card
+  #   @user = params[:user]
+  #   mail(to: @user.email, subject: "Votre période d'essai à easy Endo est terminée")
+  # end
 
-  def subscription_canceled
-    @user = params[:user]
-    mail(to: @user.email, subject: "Votre abonnement easy Endo est terminé")
-  end
+  # def subscription_canceled
+  #   @user = params[:user]
+  #   mail(to: @user.email, subject: "Votre abonnement easy Endo est terminé")
+  # end
 
-  def customer_changed_plan
-    @user = params[:user]
-    @duration = params[:duration]
-    @interval = params[:interval]
-    @subscription = params[:subscription]
-    mail(to: @user.email, subject: "Votre abonnement > easy endo en accès illimité ! ")
-  end
+  # def customer_changed_plan
+  #   @user = params[:user]
+  #   @duration = params[:duration]
+  #   @interval = params[:interval]
+  #   @subscription = params[:subscription]
+  #   mail(to: @user.email, subject: "Votre abonnement > easy endo en accès illimité ! ")
+  # end
 
-  def invoice_payment_failed
-    @user = params[:user]
-    mail(to: @user.email, subject: "Votre paiement n'a pas fonctionné")
-  end
+  # def invoice_payment_failed
+  #   @user = params[:user]
+  #   mail(to: @user.email, subject: "Votre paiement n'a pas fonctionné")
+  # end
 end
