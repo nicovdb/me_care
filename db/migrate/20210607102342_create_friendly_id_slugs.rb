9eab23f7c1e1a67efda72a9d09f0d2286749165f
claire gautier <claire.gautier@edhec.com>
2021-06-17 17:11:34 +0200

    changed migration

--- db/migrate/20210607102342_create_friendly_id_slugs.rb
+++ db/migrate/20210607102342_create_friendly_id_slugs.rb
@@ -6,19 +6,18 @@ MIGRATION_CLASS =
   end
 
 class CreateFriendlyIdSlugs < MIGRATION_CLASS
-  def up
-    drop_table(:friendly_id_slugs, if_exists: true)
-  end
-  def change
-    create_table :friendly_id_slugs do |t|
-      t.string   :slug,           :null => false
-      t.integer  :sluggable_id,   :null => false
-      t.string   :sluggable_type, :limit => 50
-      t.string   :scope
-      t.datetime :created_at
+  unless Rails.env.production?
+    def change
+      create_table :friendly_id_slugs do |t|
+        t.string   :slug,           :null => false
+        t.integer  :sluggable_id,   :null => false
+        t.string   :sluggable_type, :limit => 50
+        t.string   :scope
+        t.datetime :created_at
+      end
+      add_index :friendly_id_slugs, [:sluggable_type, :sluggable_id]
+      add_index :friendly_id_slugs, [:slug, :sluggable_type], length: { slug: 140, sluggable_type: 50 }
+      add_index :friendly_id_slugs, [:slug, :sluggable_type, :scope], length: { slug: 70, sluggable_type: 50, scope: 70 }, unique: true
     end
-    add_index :friendly_id_slugs, [:sluggable_type, :sluggable_id]
-    add_index :friendly_id_slugs, [:slug, :sluggable_type], length: { slug: 140, sluggable_type: 50 }
-    add_index :friendly_id_slugs, [:slug, :sluggable_type, :scope], length: { slug: 70, sluggable_type: 50, scope: 70 }, unique: true
   end
 end
