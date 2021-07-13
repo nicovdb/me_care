d8699a2c3227f22ab784e2967b38c7a7358e8e7b
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2021-07-13 11:35:41 +0200

    subject submission and edit  for all

--- app/policies/subject_policy.rb
+++ app/policies/subject_policy.rb
@@ -14,11 +14,11 @@ class SubjectPolicy < ApplicationPolicy
   end
 
   def create?
-    is_admin?
+    true
   end
 
   def update?
-    is_admin?
+    is_admin_or_owner?
   end
 
   def destroy?
@@ -33,5 +33,9 @@ class SubjectPolicy < ApplicationPolicy
 
   def is_admin?
     user.admin?
+  end
+
+  def is_admin_or_owner?
+    record.user == user || user.admin
   end
 end
