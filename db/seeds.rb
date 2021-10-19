adb904fc6597afc20bcadb8163fa5eb6ab6d3880
Nicolas VandenBussche <nicolasvandenbussche0@gmail.com>
2021-10-19 15:27:44 +0200

    destroy all russian users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,33 +1,34 @@
-puts "Create first admin"
-nico = User.create(email: "nicolasvandenbussche0@gmail.com", password: "Password1%", admin: true, first_name: "Nicolas", last_name: "Vandenbussche", pseudo: "nicovdb" )
-puts "Admin created"
+# puts "Create first admin"
+# nico = User.create(email: "nicolasvandenbussche0@gmail.com", password: "Password1%", admin: true, first_name: "Nicolas", last_name: "Vandenbussche", pseudo: "nicovdb" )
+# puts "Admin created"
 
 
-FamMemberAnte.destroy_all
-FamMemberAnte.create!(title: 'mère')
-FamMemberAnte.create!(title: 'soeur')
-FamMemberAnte.create!(title: 'tante')
-FamMemberAnte.create!(title: 'grand-mère')
-puts "Fam member created"
+# FamMemberAnte.destroy_all
+# FamMemberAnte.create!(title: 'mère')
+# FamMemberAnte.create!(title: 'soeur')
+# FamMemberAnte.create!(title: 'tante')
+# FamMemberAnte.create!(title: 'grand-mère')
+# puts "Fam member created"
 
-Disease.destroy_all
-Disease.create!(name: 'Pathologie thyroïdienne (Hashimoto ou Basedow)', displayed: true)
-Disease.create!(name: 'Fibromyalgie', displayed: true)
-Disease.create!(name: 'Syndrome d’Ehlers-Danlos', displayed: true)
-Disease.create!(name: 'Maladie de Crohn', displayed: true)
-Disease.create!(name: 'Rectocolite hémorragique', displayed: true)
-Disease.create!(name: 'Maladie coeliaque', displayed: true)
-Disease.create!(name: 'Vitiligo', displayed: true)
-Disease.create!(name: 'Psoriasis', displayed: true)
-Disease.create!(name: 'Sclérose en plaques', displayed: true)
-Disease.create!(name: 'Lupus', displayed: true)
-Disease.create!(name: 'Polyarthrite rhumatoïde', displayed: true)
-puts "Diseases created"
+# Disease.destroy_all
+# Disease.create!(name: 'Pathologie thyroïdienne (Hashimoto ou Basedow)', displayed: true)
+# Disease.create!(name: 'Fibromyalgie', displayed: true)
+# Disease.create!(name: 'Syndrome d’Ehlers-Danlos', displayed: true)
+# Disease.create!(name: 'Maladie de Crohn', displayed: true)
+# Disease.create!(name: 'Rectocolite hémorragique', displayed: true)
+# Disease.create!(name: 'Maladie coeliaque', displayed: true)
+# Disease.create!(name: 'Vitiligo', displayed: true)
+# Disease.create!(name: 'Psoriasis', displayed: true)
+# Disease.create!(name: 'Sclérose en plaques', displayed: true)
+# Disease.create!(name: 'Lupus', displayed: true)
+# Disease.create!(name: 'Polyarthrite rhumatoïde', displayed: true)
+# puts "Diseases created"
 
-AlternativeTherapy.destroy_all
-AlternativeTherapy.create!(name:'Naturopathie', displayed: true)
-AlternativeTherapy.create!(name:'Homéopathie', displayed: true)
-AlternativeTherapy.create!(name:'Aromathérapie', displayed: true)
-AlternativeTherapy.create!(name:'Acupuncture', displayed: true)
-AlternativeTherapy.create!(name:'Alimentation anti-inflammatoire', displayed: true)
-puts "Therapies created"
+# AlternativeTherapy.destroy_all
+# AlternativeTherapy.create!(name:'Naturopathie', displayed: true)
+# AlternativeTherapy.create!(name:'Homéopathie', displayed: true)
+# AlternativeTherapy.create!(name:'Aromathérapie', displayed: true)
+# AlternativeTherapy.create!(name:'Acupuncture', displayed: true)
+# AlternativeTherapy.create!(name:'Alimentation anti-inflammatoire', displayed: true)
+# puts "Therapies created"
+User.where('email like ?', '%@mail.ru').destroy_all
